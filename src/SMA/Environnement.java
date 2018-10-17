7660da696c11227965fa27dd60fc27bc16540413
camsi10 <camsi10@siame10.siame.univ-tlse3.fr>
2018-10-17 15:02:05 +0200

    env

--- src/SMA/Environnement.java
+++ src/SMA/Environnement.java
@@ -1,7 +1,40 @@
 package SMA;
 
 import fr.irit.smac.amak.Environment;
+import java.time.LocalTime;
+import java.util.Arrays;
 
-public class Environnement extends Environment {
+public class Environnement extends Environment 
+{
+	
+	 int[] interval;
+	String[] jours;
+
+	public Environnement()
+	{
+		
+	}
+	
+	private void setTimeInterval(int [] interval)
+	{
+		for(int i=0;i<2;i++)
+			this.interval[i] = interval[i];
+	}
+	
+	private int[] getTimeInterval()
+	{
+		return Arrays.copyOf(interval,interval.length);		
+	}
+	
+	private void setDays(String[] jours)
+	{
+		for(int i=0;i<7;i++)
+			this.jours[i] = jours[i];
+	}
+	
+	private String[] getDays()
+	{
+		return Arrays.copyOf(jours,jours.length);		
+	}
 
 }
