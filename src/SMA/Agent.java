f2512e81a9302c058836e6a1d9dfe44a234a0874
camsi11 <camsi11@siame7.siame.univ-tlse3.fr>
2018-10-17 15:46:28 +0200

    aaaaaa

---
+++ src/SMA/Agent.java
@@ -0,0 +1,39 @@
+package SMA;
+
+public class Agent{
+private int id_agent;
+
+//declaration construct Agent
+public Agent(int id_agent)
+{
+	this.id_agent=id_agent;
+}
+
+
+public int getid_agent() {
+	return id_agent;
+}
+
+public void setid_agent(int id_agent) {
+	id_agent= this.id_agent;
+}
+
+public int recuperer() {
+	return this.id_agent;
+}
+   
+
+public int envoyer() {
+	return this.id_agent;
+}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
---
+++ src/SMA/Capteur_de_presence.java
@@ -0,0 +1,27 @@
+package SMA;
+
+public class Capteur_de_presence extends CAPTEUR{
+	
+	private int CP;
+	
+
+
+	public Capteur_de_presence(String t, int d) {
+		super(t, d);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public int getCP() {
+		return CP;
+	}
+
+
+
+	public void setCP(int cP) {
+		CP = cP;
+	}
+
+
+}
