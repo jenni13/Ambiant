66b22d9bfb78d80e858fc96d699999e900b35a1d
camsi11 <camsi11@siame7.siame.univ-tlse3.fr>
2018-10-17 15:44:11 +0200

    modification agent

--- src/SMA/Environnement.java
+++ src/SMA/Environnement.java
@@ -1,7 +1,13 @@
 package SMA;
 
 import fr.irit.smac.amak.Environment;
+import fr.irit.smac.amak.Scheduling;
 
 public class Environnement extends Environment {
+
+	public Environnement(Scheduling _scheduling, Object[] params) {
+		super(_scheduling, params);
+		// TODO Auto-generated constructor stub
+	}
 
 }
